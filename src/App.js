1da023fa81dd14ea4f22c0b3c686d94ddf2d5790
Vanderlei Dallagnolo <vanderlei.dallagnolo@ellevo.com>
2023-04-11 22:43:34 -0300

    ...

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+// this import is possible because of the existence of the file
+// ./components/index.js (and the imports it contains)
+//import { Article, Brand, CTA, Feature, Navbar } from './components';
+import { Brand, CTA, Navbar } from './components';
+
+// this import is possible because of the existen of the file
+// ./containers/index.js (and the imports it contains)
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App' >
+      {/* <h1>GPT-3</h1> */}
+
+        <div className="gradient__bg">
+
+            <Navbar />
+
+            <Header />
+
+        </div>
+
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+
+    </div>
+  )
+}
+
+export default App
